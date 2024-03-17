8ee48a04fbc5c48ff0b540c1997c30ef243b4427
jorgeantonio21 <matroid@outlook.com>
2024-03-17 23:19:45 +0000

    feat: Implement InferenceConfig struct with hardware and software specifications
    - Add InferenceConfig struct with fields for hardware_specs and software_specs
    - Implement methods to access hardware and software specifications
    - Implement Default trait for InferenceConfig with default hardware and software specifications

--- atoma-inference/src/config.rs
+++ atoma-inference/src/config.rs
@@ -1,3 +1,31 @@
-pub struct InferenceConfig { 
-    
+use crate::specs::{CudaVersion, HardwareSpec, SoftwareSpec};
+
+pub struct InferenceConfig {
+    hardware_specs: HardwareSpec,
+    software_specs: SoftwareSpec,
+}
+
+impl InferenceConfig { 
+    pub fn new(hardware_specs: HardwareSpec, software_specs: SoftwareSpec) -> Self { 
+        Self { hardware_specs, software_specs }
+    }
+
+    pub fn hardware(&self) -> HardwareSpec { 
+        self.hardware_specs.clone()
+    }
+
+    pub fn software(&self) -> SoftwareSpec {
+        self.software_specs.clone()
+    }
+}
+
+impl Default for InferenceConfig {
+    fn default() -> Self {
+        InferenceConfig {
+            hardware_specs: HardwareSpec::Gpu(crate::specs::GpuModel::Nvidia(
+                crate::specs::NvidiaModel::Rtx4090(2),
+            )),
+            software_specs: SoftwareSpec::Cuda(CudaVersion::v11),
+        }
+    }
 }
