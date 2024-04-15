20c0871c272d8ddc8cdffb86ea147ef83141e9ba
jorgeantonio21 <matroid@outlook.com>
2024-04-15 10:47:38 +0100

    create bin for atoma node

--- atoma-event-subscribe/sui/src/main.rs
+++ atoma-event-subscribe/sui/src/main.rs
@@ -9,11 +9,11 @@ struct Args {
     #[arg(long)]
     pub package_id: String,
     /// HTTP node's address for Sui client
-    #[arg(long)]
-    pub http_addr: Option<String>,
+    #[arg(long, default_value = "https://fullnode.devnet.sui.io:443")]
+    pub http_addr: String,
     /// RPC node's web socket address for Sui client
-    #[arg(long)]
-    pub ws_socket_addr: Option<String>,
+    #[arg(long, default_value = "wss://fullnode.devnet.sui.io:443")]
+    pub ws_socket_addr: String,
 }
 
 #[tokio::main]
@@ -23,12 +23,8 @@ async fn main() -> Result<(), SuiSubscriberError> {
     let args = Args::parse();
     let package_id = ObjectID::from_hex_literal(&args.package_id)?;
 
-    let http_url = args
-        .http_addr
-        .unwrap_or("https://fullnode.devnet.sui.io:443".to_string());
-    let ws_url = args
-        .ws_socket_addr
-        .unwrap_or("wss://fullnode.devnet.sui.io:443".to_string());
+    let http_url = args.http_addr;
+    let ws_url = args.ws_socket_addr;
     let (event_sender, mut event_receiver) = tokio::sync::mpsc::channel(32);
 
     let event_subscriber =
