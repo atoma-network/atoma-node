0168c48b2f5930e07782fb9eaaeea443693afd6c
jorgeantonio21 <matroid@outlook.com>
2024-04-15 09:15:25 +0100

    address PR comments

--- atoma-event-subscribe/sui/src/main.rs
+++ atoma-event-subscribe/sui/src/main.rs
@@ -9,10 +9,10 @@ struct Args {
     pub package_id: String,
     /// HTTP node's address for Sui client
     #[arg(long, default_value = "https://fullnode.devnet.sui.io:443")]
-    pub http_addr: Option<String>,
+    pub http_addr: String,
     /// RPC node's web socket address for Sui client
     #[arg(long, default_value = "wss://fullnode.devnet.sui.io:443")]
-    pub ws_socket_addr: Option<String>,
+    pub ws_socket_addr: String,
 }
 
 #[tokio::main]
@@ -22,8 +22,8 @@ async fn main() -> Result<(), SuiSubscriberError> {
     let args = Args::parse();
     let package_id = ObjectID::from_hex_literal(&args.package_id)?;
 
-    let http_url = args.http_addr.unwrap();
-    let ws_url = args.ws_socket_addr.unwrap();
+    let http_url = args.http_addr;
+    let ws_url = args.ws_socket_addr;
     let event_subscriber = SuiSubscriber::new(&http_url, Some(&ws_url), package_id).await?;
     event_subscriber.subscribe().await?;
 
