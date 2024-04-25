5fc5693ac397006385484f130f46bf0bd7f4f939
jorgeantonio21 <matroid@outlook.com>
2024-04-25 16:35:43 +0100

    sui constants

--- atoma-client/src/lib.rs
+++ atoma-client/src/lib.rs
@@ -2,3 +2,9 @@ mod client;
 mod config;
 
 pub use client::{AtomaSuiClient, AtomaSuiClientError};
+
+pub struct SuiConstants { 
+    package_id: String,
+    module_id: String,
+    method: String,
+}
