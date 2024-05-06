66638bb7582a2ef83c79ae36e9860c73c8492088
jorgeantonio21 <matroid@outlook.com>
2024-05-06 09:18:14 +0100

    first commit

--- atoma-client/src/client.rs
+++ atoma-client/src/client.rs
@@ -138,13 +138,16 @@ impl AtomaSuiClient {
         let tx_digest = tx_response.digest.into_inner();
         if let Some(events) = tx_response.events {
             for event in events.data.iter() {
-                let event_value = &event.parsed_json;
-                if let Some(true) = event_value["is_first_submission"].as_bool() {
+                info!("Got a transaction event: {:?}", event.type_.name.as_str());
+                if event.type_.name.as_str() == "FirstSubmissionEvent" {
+                    info!("FJAIODJFAOSDIFJAPSODFIJAD");
                     self.output_manager_tx.send((tx_digest, response)).await?;
                     break; // we don't need to check other events, as at this point the node knows it has been selected for
                 }
+                info!("FJAIODJFAOSDIFJAPSODFIJAD");
             }
         }
+        info!("FJAIODJFAOSDIFJAPSODFIJAD");
         Ok(tx_digest)
     }
 
