eb215d382a2c218e0c08f3b6d2a2fc67abfc3a6b
Martin Stefcek <gcifko@gmail.com>
2024-05-16 10:10:23 +0400

    feat: merge config

--- atoma-node/src/main.rs
+++ atoma-node/src/main.rs
@@ -7,13 +7,7 @@ const CHANNEL_BUFFER: usize = 32;
 #[derive(Debug, Parser)]
 struct Args {
     #[arg(long)]
-    atoma_sui_client_config_path: String,
-    #[arg(long)]
-    model_config_path: String,
-    #[arg(long)]
-    sui_subscriber_path: String,
-    #[arg(long)]
-    output_manager_config_path: String,
+    config_path: String,
 }
 
 #[tokio::main]
@@ -21,20 +15,10 @@ async fn main() -> Result<(), AtomaNodeError> {
     tracing_subscriber::fmt::init();
 
     let args = Args::parse();
-    let atoma_sui_client_config_path = args.atoma_sui_client_config_path;
-    let model_config_path = args.model_config_path;
-    let sui_subscriber_path = args.sui_subscriber_path;
-    let output_manager_config_path = args.output_manager_config_path;
+    let config_path = args.config_path;
 
     let (_, json_rpc_server_rx) = mpsc::channel(CHANNEL_BUFFER);
-    AtomaNode::start(
-        atoma_sui_client_config_path,
-        model_config_path,
-        sui_subscriber_path,
-        output_manager_config_path,
-        json_rpc_server_rx,
-    )
-    .await?;
+    AtomaNode::start(config_path, json_rpc_server_rx).await?;
 
     Ok(())
 }
