f9fb5ec09e35ba7285beb4552cceb27c5589038c
jorgeantonio21 <matroid@outlook.com>
2024-06-10 10:28:15 +0100

    fmt

--- atoma-vllm/src/models.rs
+++ atoma-vllm/src/models.rs
@@ -3,11 +3,13 @@ use async_trait::async_trait;
 use crate::sampling_params::SamplingParams;
 
 #[async_trait]
-pub trait ModelLoader { 
+pub trait ModelLoader {
     type Error;
 
     async fn fetch();
-    async fn load() -> Result<Self, Self::Error> where Self: Sized;
+    async fn load() -> Result<Self, Self::Error>
+    where
+        Self: Sized;
 }
 
 #[async_trait]
@@ -17,6 +19,9 @@ pub trait ModelExecutor: ModelLoader {
     type Output;
 
     async fn forward(&mut self, input: Self::Input) -> Result<Self::Logits, Self::Error>;
-    async fn sample(&mut self, input: Self::Logits, sampling_params: SamplingParams) -> Result<Self::Output, Self::Error>;
+    async fn sample(
+        &mut self,
+        input: Self::Logits,
+        sampling_params: SamplingParams,
+    ) -> Result<Self::Output, Self::Error>;
 }
-
