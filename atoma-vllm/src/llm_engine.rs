5014c3f62181ebc1f82d9a105533220d5be1f185
Jorge António <matroid@outlook.com>
2024-06-19 16:38:54 +0100

    feat: atoma-vllm (#104)
    * first commit
    
    * first commit
    
    * clippy
    
    * update logic for BlockAllocator
    
    * update comments
    
    * resolve issue
    
    * add tests
    
    * uncached logic and test
    
    * start sequence logic
    
    * first commit
    
    * add tests for functionality
    
    * lints clippy
    
    * lints clippy
    
    * add additional logic for block manager
    
    * add logic for swap out
    
    * add further logic
    
    * add tests and resolve bugs
    
    * fmt Cargo.toml
    
    * address some PR comments
    
    * address further PR comments
    
    * fmt
    
    * add tracing error
    
    * address further PR comments
    
    * feat: vllm scheduler (#105)
    
    * first commit
    
    * add further logic
    
    * finish logic for schedule_running
    
    * add logic for schedule_swap
    
    * add prefill schedule logic
    
    * add logic for schedule prefill
    
    * start schedule default implementation
    
    * add schedule default logic
    
    * schdule method, init scheduled_chunked_prefill
    
    * handle logic for schedule prefill chunk
    
    * add logic to schedule method
    
    * starting arc<mutex<>> for sequence groups in queues
    
    * initial steps into using Rc<RefCell<>>
    
    * refactor to Rc<RefCell<T>> sequence group sequences for shared access and resolve major bug
    
    * add test
    
    * resolve multiple bugs
    
    * add test
    
    * add tests
    
    * add more tests
    
    * add tests
    
    * add final tests
    
    * made changes
    
    * minor changes
    
    * add small changes
    
    * address PR comments
    
    * feat: input validation and tokenization (#114)
    
    * first commit
    
    * add further logic
    
    * finish logic for schedule_running
    
    * add logic for schedule_swap
    
    * add prefill schedule logic
    
    * add logic for schedule prefill
    
    * start schedule default implementation
    
    * add schedule default logic
    
    * schdule method, init scheduled_chunked_prefill
    
    * handle logic for schedule prefill chunk
    
    * add logic to schedule method
    
    * starting arc<mutex<>> for sequence groups in queues
    
    * initial steps into using Rc<RefCell<>>
    
    * refactor to Rc<RefCell<T>> sequence group sequences for shared access and resolve major bug
    
    * add test
    
    * resolve multiple bugs
    
    * add test
    
    * add tests
    
    * add more tests
    
    * add tests
    
    * add final tests
    
    * made changes
    
    * validation process
    
    * add changes
    
    * lints
    
    * address PR comments
    
    * feat: llm engine first steps (#117)
    
    * first commit
    
    * add further logic
    
    * finish logic for schedule_running
    
    * add logic for schedule_swap
    
    * add prefill schedule logic
    
    * add logic for schedule prefill
    
    * start schedule default implementation
    
    * add schedule default logic
    
    * schdule method, init scheduled_chunked_prefill
    
    * handle logic for schedule prefill chunk
    
    * add logic to schedule method
    
    * starting arc<mutex<>> for sequence groups in queues
    
    * initial steps into using Rc<RefCell<>>
    
    * refactor to Rc<RefCell<T>> sequence group sequences for shared access and resolve major bug
    
    * add test
    
    * resolve multiple bugs
    
    * add test
    
    * add tests
    
    * add more tests
    
    * add tests
    
    * add final tests
    
    * made changes
    
    * validation process
    
    * add changes
    
    * first commit
    
    * fmt
    
    * add model executor
    
    * first commit
    
    * add logic for handling the llm engine and model executor in the background
    
    * add histograms for output generation metrics
    
    * add changes
    
    * refactor rc<refcell<>> to arc<rwlock<>> for async code
    
    * add llm service logic
    
    * clippy
    
    * address PR comments
    
    * test: llm engine testing (#121)
    
    * first commit
    
    * add batched requests
    
    * add changes
    
    * fmt
    
    * remove tokenizer
    
    * add changes
    
    * small changes
    
    * add skip all to scheduling algorithms
    
    * change tokenizer dependency
    
    * fmt
    
    * address PR changes
    
    * fmt

---
+++ atoma-vllm/src/llm_engine.rs
@@ -0,0 +1,465 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+    time::{Duration, Instant},
+};
+
+use futures::StreamExt;
+use thiserror::Error;
+use tokenizers::Tokenizer;
+use tokio::sync::{
+    mpsc::{error::SendError, UnboundedReceiver, UnboundedSender},
+    oneshot::error::RecvError,
+};
+use tracing::{error, info, instrument};
+
+use crate::{
+    model_executor::ModelThreadDispatcher,
+    policy::FcfsPolicy,
+    scheduler::{Scheduler, SchedulerError, SchedulerOutputs},
+    sequence::{
+        ExecuteModelRequest, LogProb, RequestMetrics, Sequence, SequenceError, SequenceGroup,
+        SequenceGroupMetadata, SequenceGroupOutput, SequenceOutput, SequenceStatus,
+    },
+    types::{ReadLock, WriteLock},
+    validation::StoppingCriteriaParameters,
+};
+
+/// Time in milliseconds we wait until we schedule new received requests,
+/// in case the `LlmEngine` was on halt.
+const SCHEDULE_WAIT_PERIOD: u64 = 100;
+
+/// `LlmEngine` - An asynchronous worker
+/// to handle scheduling new requests. It is also responsible
+/// to communicate with the `ModelExecutor` service to send new requests
+/// for batched AI inference
+pub struct LlmEngine {
+    /// Atoma's client sender channel, to share newly AI
+    /// generated outputs
+    atoma_client_sender: UnboundedSender<Vec<GenerateRequestOutput>>,
+    /// End of sentence token, for the current model's tokenizer
+    eos_token_id: u32,
+    /// Model executor, responsible for running
+    /// decoding steps to produce AI generated outputs
+    model_thread_dispatcher: ModelThreadDispatcher,
+    /// Receiver responsible for receiving new
+    /// requests from the `LllmEngine` service
+    request_receiver: UnboundedReceiver<SequenceGroup>,
+    /// Current scheduled `SequenceGroup`'s metadata
+    sequence_groups_metadata: Vec<Arc<SequenceGroupMetadata>>,
+    /// Current `SchedulerOutputs`
+    scheduler_outputs: SchedulerOutputs,
+    /// A `Scheduler` instance
+    scheduler: Scheduler<FcfsPolicy>,
+    /// Tokenizer for decoding sequences
+    tokenizer: Tokenizer,
+}
+
+impl LlmEngine {
+    /// Constructor
+    pub fn new(
+        atoma_client_sender: UnboundedSender<Vec<GenerateRequestOutput>>,
+        eos_token_id: u32,
+        model_thread_dispatcher: ModelThreadDispatcher,
+        request_receiver: UnboundedReceiver<SequenceGroup>,
+        scheduler: Scheduler<FcfsPolicy>,
+        tokenizer: Tokenizer,
+    ) -> Self {
+        Self {
+            atoma_client_sender,
+            eos_token_id,
+            model_thread_dispatcher,
+            sequence_groups_metadata: vec![],
+            scheduler_outputs: SchedulerOutputs::create_empty(),
+            scheduler,
+            tokenizer,
+            request_receiver,
+        }
+    }
+
+    /// Main loop -
+    ///     1. Listens to incoming requests and adds these to the underlying
+    ///         `Scheduler`.
+    ///     2. Awaits until new outputs are generated from the `ModelExecutor`
+    ///         service. It then processes the outputs to update the associated
+    ///         `SequenceGroup` states and re-schedules new requests.
+    ///     3. Sends finished `SequenceGroup` outputs to the Atoma's client
+    ///         service.
+    #[instrument(skip(self))]
+    pub async fn run(mut self) -> Result<(), EngineError> {
+        loop {
+            tokio::select! {
+                Some(sequence_group) = self.request_receiver.recv() => {
+                    // 1. Adds the received `SequenceGroup` to the `Scheduler` instance.
+                    self.scheduler.add_sequence_group(sequence_group);
+
+                    // 2. If the current `LlmInstance` doesn't have any on-going
+                    //    scheduled sequence groups, we wait some time and then
+                    //    schedule all the received requests so far.
+                    //    This includes the request added in 1.
+                    if self.sequence_groups_metadata.is_empty() && self.scheduler_outputs.is_empty() {
+                        tokio::time::sleep(Duration::from_millis(SCHEDULE_WAIT_PERIOD)).await;
+                        self.step()?;
+                    }
+                },
+                Some(outputs) = self.model_thread_dispatcher.responses.next() => {
+                    self.handle_outputs(outputs.map_err(EngineError::RecvError)).await?;
+                }
+                else => {
+                    continue;
+                }
+            }
+        }
+    }
+
+    /// Handles newly AI generated `SequenceGroupOutput`'s
+    #[instrument(skip_all)]
+    async fn handle_outputs(
+        &mut self,
+        outputs: Result<Vec<SequenceGroupOutput>, EngineError>,
+    ) -> Result<(), EngineError> {
+        match outputs {
+            Ok(outputs) => {
+                // 1. Processes the newly AI generated outputs
+                let request_outputs = self.process_generated_outputs(outputs)?;
+
+                // 2. Schedules new requests
+                self.step()?;
+
+                // 3. After scheduling new requests to the `ModelExecutor`
+                //    we can send the finished outputs to the atoma client
+                //    service.
+                // NOTE: This is after scheduling new sequences above,
+                //    we do so to optimize GPU utilization. This is
+                //    supposed to be safe
+                if !request_outputs.is_empty() {
+                    self.atoma_client_sender.send(request_outputs)?;
+                }
+            }
+            Err(e) => {
+                error!("Invalid generated outputs with error: {e}");
+                // NOTE: In order to maintain the system live, we need to keep calling
+                // the `self.step()` method, even in possible scenarios of failure.
+                self.step()?;
+            }
+        }
+        Ok(())
+    }
+
+    /// Main method of `LlmEngine`.
+    ///
+    /// 1. It is responsible for scheduling new
+    ///     requests, via the associated `Scheduler`. Once scheduling is complete,
+    ///
+    /// 2. It sends a new `ExecuteModelRequest` to the `ModelExecutor`'s thread.
+    #[instrument(skip(self))]
+    pub fn step(&mut self) -> Result<(), EngineError> {
+        // 1. Schedule new requests
+        let (sequence_groups_metadata, scheduler_outputs) = self.scheduler.schedule()?;
+
+        // 2. Update `self.scheduler_groups_metadata` and `scheduler_outputs`
+        self.sequence_groups_metadata = sequence_groups_metadata.clone();
+        self.scheduler_outputs = scheduler_outputs.clone();
+
+        // 3. If the scheduled data is empty, it means that
+        //     no new requests were received.
+        if scheduler_outputs.is_empty() {
+            return Ok(());
+        }
+
+        let execute_model_request = ExecuteModelRequest::new(
+            sequence_groups_metadata,
+            scheduler_outputs.blocks_to_swap_in,
+            scheduler_outputs.blocks_to_swap_out,
+            scheduler_outputs.blocks_to_copy,
+            scheduler_outputs.running_queue_size,
+        );
+
+        // 4. Sends a new `ExecuteModelRequest` to the underlying `ModelExecutor`'s thread
+        self.model_thread_dispatcher.send(execute_model_request);
+
+        Ok(())
+    }
+
+    /// Processes a newly AI generated output
+    #[instrument(skip_all)]
+    fn process_generated_outputs(
+        &mut self,
+        outputs: Vec<SequenceGroupOutput>,
+    ) -> Result<Vec<GenerateRequestOutput>, EngineError> {
+        let now = Instant::now();
+
+        for (output, (sequence_group_metadata, scheduled_sequence_group)) in outputs.iter().zip(
+            self.sequence_groups_metadata
+                .iter()
+                .zip(self.scheduler_outputs.scheduled_sequence_groups.iter()),
+        ) {
+            // 1. Update the number of computed tokens for scheduled `SequenceGroup`
+            scheduled_sequence_group
+                .scheduled_group
+                .update_num_computed_tokens(scheduled_sequence_group.token_chunk_size)?;
+
+            let stopping_criteria_params =
+                scheduled_sequence_group.scheduled_group.stopping_params();
+
+            // 2. Iterate over each `Sequence`s of `ScheduledSequenceGroup` and update its current state
+            // after the new LLM inference iteration has been performed
+            for (sequence_id, sequence) in scheduled_sequence_group.scheduled_group.sequences.iter()
+            {
+                let sequence_output = if let Some(output) = output.outputs.get(sequence_id) {
+                    output
+                } else {
+                    error!(
+                        "Missing generated sequence output token for sequence with id = {}",
+                        sequence_id
+                    );
+                    return Err(EngineError::MissingSequenceOutputToken(*sequence_id));
+                };
+
+                // 3. Updates the state of the current `Sequence`
+                self.update_sequence(
+                    sequence,
+                    sequence_output,
+                    sequence_group_metadata,
+                    &stopping_criteria_params,
+                )?;
+            }
+
+            // 4. Add a few metrics
+            let metrics_guard = scheduled_sequence_group
+                .scheduled_group
+                .metrics
+                .read()
+                .unwrap();
+
+            let arrival_time_histogram = metrics::histogram!("sequence-group-arrival-time");
+            arrival_time_histogram.record(metrics_guard.arrival_time.elapsed().as_secs_f32());
+
+            let last_token_time_histogram = metrics::histogram!("sequence-group-last-token-time");
+            last_token_time_histogram.record(metrics_guard.last_token_time.elapsed().as_secs_f32());
+        }
+
+        // 5. Free all finished sequence groups
+        self.scheduler.free_finished_sequence();
+
+        // 6. Keep track of all the finished `SequenceGroup`s
+        let mut request_outputs = Vec::new();
+        for scheduled_sequence_group in self.scheduler_outputs.scheduled_sequence_groups.iter() {
+            scheduled_sequence_group
+                .scheduled_group
+                .maybe_set_first_scheduled_time(now);
+
+            if scheduled_sequence_group.scheduled_group.is_finished() {
+                request_outputs.push(GenerateRequestOutput::from_sequence_group(
+                    &scheduled_sequence_group.scheduled_group,
+                ));
+            }
+        }
+        for sequence_group in self.scheduler_outputs.ignored_seq_groups.iter() {
+            sequence_group.maybe_set_first_scheduled_time(now);
+        }
+
+        Ok(request_outputs)
+    }
+
+    /// Updates the state of a `Sequence` after an LLM inference loop
+    #[instrument(skip_all)]
+    fn update_sequence(
+        &self,
+        sequence: &Arc<RwLock<Sequence>>,
+        sequence_output: &SequenceOutput,
+        sequence_group_metadata: &SequenceGroupMetadata,
+        stopping_criteria_params: &StoppingCriteriaParameters,
+    ) -> Result<(), EngineError> {
+        let sequence_id = { sequence.read_lock()?.sequence_id() };
+        // 1. Get the AI generated next output token id.
+        let generated_token_id = sequence_output.output_token;
+
+        if sequence_group_metadata.do_sample {
+            let mut sequence_guard_lock = sequence.write_lock()?;
+            // NOTE: this means we are in decoding phase.
+            // That is, we are generating new output tokens
+            // and these should be added to the `Sequence`'s
+            // state.
+
+            // 2. Update the `Sequence`'s output log-probabilities.
+            //
+            // 3. Update the `Sequence`'s `SequenceData` cumulative probabilities,
+            //    if we are in decoding phase.
+            //
+            // 4. Update the `Sequence`'s `SequenceData` output tokens,
+            //    if we are in decoding phase.
+            sequence_guard_lock
+                .add_token_id(generated_token_id, sequence_output.logprob.clone())?;
+
+            // 5. Decode the generated output token id.
+            let generated_token = self
+                .tokenizer
+                .decode(&[generated_token_id], true)
+                .map_err(|e| EngineError::TokenizerError(e.to_string()))?;
+
+            // 6. Update the `output_text` with the newly generated token,
+            //    if in decoding phase.
+            sequence_guard_lock.output_text.push_str(&generated_token);
+
+            // 7. Check if the last generated token is a stop token.
+            //    If so, update the `Sequence`'s `SequenceState` and
+            //    the `stop_reason`, as well.
+            if stopping_criteria_params
+                .stop_sequences
+                .contains(&generated_token)
+            {
+                info!("Current sequence with id = {sequence_id} has finished execution due to stopping token = {generated_token}");
+                {
+                    sequence_guard_lock.stop_reason = Some(generated_token_id)
+                }
+
+                sequence_guard_lock.set_sequence_status(SequenceStatus::FinishedStopped)
+            }
+
+            // 8. Check if the current `Sequence` last generated token
+            //    id equals to the `eos_token_id`, in which case the
+            //    the `Sequence`'s status should become `FinishedStopped`.
+            if self.eos_token_id == generated_token_id && !stopping_criteria_params.ignore_eos_token
+            {
+                sequence_guard_lock.set_sequence_status(SequenceStatus::FinishedStopped)
+            }
+
+            // 9. Check if the `Sequence`'s length exceeds that of
+            //     `SchedulerConfig`'s. If so, update the `Sequence`'s
+            //     `SequenceStatus` to `FinishedLengthCapped`.
+            let sequence_len = sequence_guard_lock.length();
+            if sequence_len > self.scheduler.scheduler_config.max_model_len() {
+                sequence_guard_lock.set_sequence_status(SequenceStatus::FinishedLengthCapped)
+            }
+
+            // 10. Check if the `Sequence`'s output length exceeds that of
+            //     Request's `max_new_tokens`.
+            let sequence_output_len = sequence_guard_lock.get_output_len();
+            if sequence_output_len > stopping_criteria_params.max_new_tokens as usize {
+                sequence_guard_lock.set_sequence_status(SequenceStatus::FinishedLengthCapped)
+            }
+
+            // 11. Update the `Sequence`'s tokens vec.
+            sequence_guard_lock.tokens.push(generated_token)
+        } else {
+            // NOTE: in this case, we are not sampling newly
+            // generated tokens. That is, we are in prefill
+            // phase (possibly while chunking). For this reason,
+            // we do not have to add tokens to the current
+            // `Sequence`'s state.
+
+            // 2. Update the `Sequence`'s output log-probabilities.
+            sequence
+                .write_lock()?
+                .output_logprobs
+                .push(sequence_output.logprob.clone());
+        }
+
+        Ok(())
+    }
+}
+
+/// `RequestOutput` - Output of running AI inference over a `SequenceGroup`
+#[derive(Debug)]
+pub struct GenerateRequestOutput {
+    /// Request id
+    pub request_id: String,
+    /// The `String` prompt
+    pub prompt: String,
+    /// Inference outputs
+    pub inference_outputs: Vec<InferenceOutput>,
+    /// Prompt token ids
+    pub prompt_token_ids: Vec<u32>,
+    /// Is finished
+    pub is_finished: bool,
+    /// Metrics
+    pub metrics: Arc<RwLock<RequestMetrics>>,
+}
+
+impl GenerateRequestOutput {
+    /// Creates a new `Self` instance from a `SequenceGroup`
+    pub fn from_sequence_group(sequence_group: &SequenceGroup) -> Self {
+        let mut sequences = sequence_group.sequences.values().collect::<Vec<_>>();
+
+        let top_n_sequences = if sequences.len() == 1 {
+            sequences
+        } else {
+            // Get top n sequences
+            let n = sequence_group.next_token_chooser_params().n;
+            sequences.sort_by(|s1, s2| {
+                s1.read()
+                    .unwrap()
+                    .cumulative_logprob()
+                    .partial_cmp(&s2.read().unwrap().cumulative_logprob())
+                    .unwrap()
+            });
+            sequences[..n].to_vec()
+        };
+
+        let inference_outputs = top_n_sequences
+            .iter()
+            .enumerate()
+            .map(|(i, s)| {
+                let s = s.read().unwrap();
+                InferenceOutput {
+                    index: i,
+                    output_text: s.get_output_text(),
+                    token_ids: s.get_token_ids(),
+                    cumulative_logprob: s.cumulative_logprob(),
+                    logprobs: s.output_logprobs.clone(),
+                    finish_reason: s.get_sequence_status().finished_reason(),
+                    stop_reason: s.stop_reason,
+                }
+            })
+            .collect::<Vec<_>>();
+
+        Self {
+            request_id: sequence_group.request_id.clone(),
+            inference_outputs,
+            prompt: sequence_group.prompt(),
+            prompt_token_ids: sequence_group.prompt_token_ids(),
+            is_finished: sequence_group.is_finished(),
+            metrics: sequence_group.metrics.clone(),
+        }
+    }
+}
+
+/// `InferenceOutput` - Output of running a
+#[derive(Debug)]
+pub struct InferenceOutput {
+    /// The index of the output in the request
+    pub index: usize,
+    /// The generated output text
+    pub output_text: String,
+    /// The token ids of the generated output text
+    pub token_ids: Vec<u32>,
+    /// The cumulative log probability of the generated
+    /// output text
+    pub cumulative_logprob: f32,
+    /// The log probabilities of the top probability words at each
+    /// position if the logprobs are requested
+    pub logprobs: Vec<HashMap<u32, LogProb>>,
+    /// The reason why the sequence is finished
+    pub finish_reason: Option<String>,
+    /// The stop token id that caused the completion
+    /// to stop, None if the completion finished for some other reason
+    /// including encountering the eos token
+    pub stop_reason: Option<u32>,
+}
+
+#[derive(Debug, Error)]
+pub enum EngineError {
+    #[error("Scheduler error: `{0}`")]
+    SchedulerError(#[from] SchedulerError),
+    #[error("Sequence error: `{0}`")]
+    SequenceError(#[from] SequenceError),
+    #[error("Missing sequence output token, id = `{0}`")]
+    MissingSequenceOutputToken(u64),
+    #[error("Tokenizer error: `{0}`")]
+    TokenizerError(String),
+    #[error("Send error: `{0}`")]
+    SendError(#[from] SendError<Vec<GenerateRequestOutput>>),
+    #[error("Recv error: `{0}`")]
+    RecvError(#[from] RecvError),
+}
---
+++ atoma-vllm/src/model_executor.rs
@@ -0,0 +1,237 @@
+use std::{collections::HashMap, path::PathBuf};
+
+use async_trait::async_trait;
+use futures::stream::FuturesUnordered;
+use thiserror::Error;
+use tokio::{
+    sync::{
+        mpsc,
+        oneshot::{self, error::RecvError},
+    },
+    task::JoinHandle,
+};
+use tracing::{error, info, instrument, trace};
+
+use crate::{
+    sequence::{ExecuteModelRequest, LogProb, SequenceGroupOutput, SequenceOutput},
+    validation::{NextTokenChooserParameters, StoppingCriteriaParameters},
+};
+
+#[async_trait]
+/// `ModelLoader` trait - interface for fetching
+/// and loading a LLM model weights. Also has a method
+/// providing the `eos_token_id` for the current model's
+/// tokenizer.
+pub trait ModelLoader {
+    type FilePaths;
+
+    async fn fetch() -> Result<Self::FilePaths, ModelLoaderError>;
+    async fn load() -> Result<Self, ModelLoaderError>
+    where
+        Self: Sized;
+    fn cache_dir(&self) -> PathBuf;
+    fn eos_token_id(&self) -> Option<u32>;
+}
+
+/// `ModelExecutor` trait - interface for running AI inference
+/// from a LLM
+pub trait ModelExecutor: ModelLoader {
+    type Input: From<ExecuteModelRequest>;
+    type Logits: Clone;
+    type Output: Into<u32>;
+
+    fn forward(&mut self, input: Self::Input) -> Result<Self::Logits, ModelExecutorError>;
+    fn sample(
+        &mut self,
+        input: Self::Logits,
+        next_token_params: NextTokenChooserParameters,
+        stopping_params: StoppingCriteriaParameters,
+    ) -> Result<Self::Output, ModelExecutorError>;
+}
+
+/// `ModelThreadCommand` - encapsulates a `ValidGenerateRequest`
+/// to run AI inference on, together with a oneshot::Sender
+/// channel to communicate the AI generated output with the
+/// main task
+pub struct ModelThreadCommand {
+    request: ExecuteModelRequest,
+    sender: oneshot::Sender<Vec<SequenceGroupOutput>>,
+}
+
+/// `ModelThread` - encapsulates the logic
+/// to run a model thread/task in the background.
+/// It receives new coming requests and start processing
+/// AI inference on it.
+pub struct ModelThread<M: ModelExecutor> {
+    model: M,
+    receiver: mpsc::UnboundedReceiver<ModelThreadCommand>,
+}
+
+impl<M> ModelThread<M>
+where
+    M: ModelExecutor + Send + Sync,
+{
+    /// Main loop, it listenings to incoming requests, in the form `ModelThreadCommand`.
+    /// When a new request is received, it starts a new inference loop for the encapsulated
+    /// AI model `M`. Once the AI generated output is ready, it sends it back using the corresponding
+    /// `oneshot` `Sender` encapsulated in the `ModelThreadCommand`.
+    #[instrument(skip(self))]
+    pub fn run(mut self) -> Result<(), ModelThreadError> {
+        info!("Start Model thread");
+
+        while let Some(command) = self.receiver.blocking_recv() {
+            let ModelThreadCommand { request, sender } = command;
+
+            let sequence_groups_metadata = request.sequence_groups_metadata.clone();
+            let next_token_chooser_params: Vec<NextTokenChooserParameters> = request
+                .sequence_groups_metadata
+                .iter()
+                .map(|s| s.next_token_chooser_params.clone())
+                .collect();
+            let stopping_params: Vec<StoppingCriteriaParameters> = request
+                .sequence_groups_metadata
+                .iter()
+                .map(|s| s.stopping_criteria_params.clone())
+                .collect();
+
+            let logits = match self.model.forward(request.into()) {
+                Ok(logits) => logits,
+                Err(e) => {
+                    error!("Failed to run forward pass on model, with error: {e}");
+                    return Err(ModelThreadError::ModelExecutorError(e));
+                }
+            };
+
+            let mut responses = Vec::with_capacity(next_token_chooser_params.len());
+
+            // TODO: should we parallelize this loop, with rayon or within the async runtime ?
+            for (next_token_params, (stopping_params, metadata)) in next_token_chooser_params
+                .iter()
+                .zip(stopping_params.iter().zip(sequence_groups_metadata))
+            {
+                let mut outputs = HashMap::with_capacity(metadata.sequence_data.len());
+
+                for sequence_id in metadata.sequence_data.keys() {
+                    let decode_token = match self.model.sample(
+                        logits.clone(),
+                        next_token_params.clone(),
+                        stopping_params.clone(),
+                    ) {
+                        Ok(token) => token,
+                        Err(e) => {
+                            error!("Failed to sample next decoding token, with error: {e}");
+                            return Err(ModelThreadError::ModelExecutorError(e));
+                        }
+                    };
+
+                    let output_token = decode_token.into();
+
+                    outputs.insert(
+                        *sequence_id,
+                        SequenceOutput {
+                            parent_sequence_id: *sequence_id,
+                            output_token,
+                            logprob: HashMap::from_iter([(
+                                output_token,
+                                LogProb::new(0.8, None, None),
+                            )]), // TODO: replace hardcoded values with logic
+                        },
+                    );
+                }
+
+                // TODO: Check this is the correct logic, once we integrate
+                // with model executor
+                let response = SequenceGroupOutput {
+                    outputs,
+                    sampled_token_ids: None,
+                    sampled_token_probs: None,
+                    logprobs: None,
+                    spec_decode_worker_metrics: None,
+                };
+                responses.push(response);
+            }
+
+            sender.send(responses).ok();
+        }
+
+        Ok(())
+    }
+}
+
+/// `ModelThreadDispatcher` - Responsible for managing incoming requests to
+/// different the background LLM inference task
+pub struct ModelThreadDispatcher {
+    /// Mapping from each model id to the remove `Sender`'s `ModelThreadCommand`
+    pub sender: mpsc::UnboundedSender<ModelThreadCommand>,
+    /// A `FuturesUnordered` containing each generated `Response`'s oneshot receiver.
+    /// It should yield everytime a new AI inference output is generated.
+    pub responses: FuturesUnordered<oneshot::Receiver<Vec<SequenceGroupOutput>>>,
+    /// The model's thread join handle
+    pub join_handle: JoinHandle<Result<(), ModelThreadError>>,
+}
+
+impl ModelThreadDispatcher {
+    /// Starts a new instance of a `ModelThreadDispatcher`. It further spawns a new thread model
+    /// that continuously listens to incoming AI inference requests, and processes these.
+    #[instrument(skip(model))]
+    pub(crate) fn start<M>(model: M) -> Result<Self, ModelThreadError>
+    where
+        M: ModelExecutor + Send + Sync + 'static,
+    {
+        let (sender, receiver) = mpsc::unbounded_channel();
+
+        let join_handle = tokio::task::spawn_blocking(|| {
+            let model_thread = ModelThread { model, receiver };
+            if let Err(e) = model_thread.run() {
+                error!("Model thread error: {e}");
+                if !matches!(e, ModelThreadError::Shutdown(_)) {
+                    panic!("Fatal error occurred: {e}");
+                }
+            }
+
+            Ok(())
+        });
+
+        let model_dispatcher = ModelThreadDispatcher {
+            sender,
+            responses: FuturesUnordered::new(),
+            join_handle,
+        };
+
+        Ok(model_dispatcher)
+    }
+
+    /// Sends a `ModelThreadCommand` instance into the corresponding
+    /// `Model`'s thread, to be processed by the `Model` itself.
+    #[instrument(skip(self))]
+    pub fn send(&self, request: ExecuteModelRequest) {
+        trace!("Sending new `ExecuteModelRequest` to model executor task");
+
+        let (sender, receiver) = oneshot::channel();
+        let command = ModelThreadCommand { request, sender };
+
+        if let Err(e) = self.sender.send(command) {
+            error!("Could not send command to model core, it might be shutting down: {e}");
+        }
+
+        self.responses.push(receiver);
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ModelThreadError {
+    #[error("Core thread shutdown: `{0}`")]
+    Shutdown(RecvError),
+    #[error("Send error")]
+    SendError,
+    #[error("Model loader error: `{0}`")]
+    ModelLoaderError(#[from] ModelLoaderError),
+    #[error("Model executor error: `{0}`")]
+    ModelExecutorError(#[from] ModelExecutorError),
+}
+
+#[derive(Debug, Error)]
+pub enum ModelLoaderError {}
+
+#[derive(Debug, Error)]
+pub enum ModelExecutorError {}
