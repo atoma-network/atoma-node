01ed76152245ddd359dac2a9d90d60ea365436fa
jorgeantonio21 <matroid@outlook.com>
2024-10-22 14:08:06 +0100

    add sui signature logic

--- atoma-service/src/server.rs
+++ atoma-service/src/server.rs
@@ -12,6 +12,7 @@ use axum::{
 use reqwest::Client;
 use serde_json::{json, Value};
 use sqlx::SqlitePool;
+use sui_keys::keystore::FileBasedKeystore;
 use tokenizers::Tokenizer;
 use tokio::{net::TcpListener, signal, sync::watch::Sender};
 use tower::ServiceBuilder;
@@ -36,6 +37,12 @@ pub struct AppState {
 
     /// URL of the node's inference service.
     pub inference_service_client: Client,
+
+    /// The Sui keystore of the node
+    pub keystore: Arc<FileBasedKeystore>,
+
+    /// The Sui address index of the node within the keystore values
+    pub address_index: usize,
 }
 
 /// Creates and configures the main router for the application.
@@ -138,10 +145,19 @@ pub async fn chat_completions_handler(
             error!("Error sending request to inference service: {}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
-    let response_body = response.json::<Value>().await.map_err(|e| {
+    let mut response_body = response.json::<Value>().await.map_err(|e| {
         error!("Error reading response body: {}", e);
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
+
+    // Sign the response body byte content and add the base64 encoded signature to the response body
+    let signature = utils::sign_response_body(&response_body, &state.keystore, state.address_index)
+        .map_err(|e| {
+            error!("Error signing response body: {}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+    response_body["signature"] = json!(signature);
+
     // Extract the response total number of tokens
     let total_tokens = response_body
         .get("usage")
@@ -160,18 +176,27 @@ pub async fn chat_completions_handler(
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
 
-    // Sign the response body
-
     Ok(Json(response_body))
 }
 
 pub(crate) mod utils {
     use super::*;
+    use sha2::Digest;
+    use sui_keys::keystore::AccountKeystore;
+    use sui_sdk::types::crypto::EncodeDecodeBase64;
 
     pub(crate) fn sign_response_body(
-        response_body: Value,
-        public_keys: Vec<String>,
+        response_body: &Value,
+        keystore: &Arc<FileBasedKeystore>,
+        address_index: usize,
     ) -> Result<String, Box<dyn std::error::Error>> {
-        Ok(response_body.to_string())
+        let address = keystore.addresses()[address_index];
+        let response_body_str = response_body.to_string();
+        let response_body_bytes = response_body_str.as_bytes();
+        let sha256_hash = sha2::Sha256::digest(response_body_bytes);
+        let signature = keystore
+            .sign_hashed(&address, sha256_hash.as_slice())
+            .unwrap();
+        Ok(signature.encode_base64())
     }
 }
