2c65504d736d35044ade3bb48b8c07c2cb7d461a
jorgeantonio21 <matroid@outlook.com>
2024-11-08 18:24:55 +0000

    add changes

--- atoma-service/src/server.rs
+++ atoma-service/src/server.rs
@@ -392,6 +392,7 @@ async fn handle_non_streaming_response(
             },
         )?;
     response_body["signature"] = json!(signature);
+    response_body["response_hash"] = json!(response_hash);
 
     // Extract the response total number of tokens
     let total_tokens = response_body
