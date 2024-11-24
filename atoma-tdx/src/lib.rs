3b5929e105bac67da75a6aff39737beab3deb8e8
jorgeantonio21 <matroid@outlook.com>
2024-11-24 11:46:08 +0000

    first commit

--- atoma-tdx/src/lib.rs
+++ atoma-tdx/src/lib.rs
@@ -2,6 +2,8 @@
 pub mod key_rotation;
 #[cfg(feature = "tdx")]
 pub mod service;
+#[cfg(feature = "tdx")]
+pub mod encryption;
 
 #[cfg(feature = "tdx")]
 use dcap_rs::types::quotes::body::QuoteBody;
