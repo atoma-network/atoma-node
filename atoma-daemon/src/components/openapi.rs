d913bd82e52aac2cf5b20fc4e4043670d277d16f
francis2tm <franciscomelot@hotmail.com>
2024-12-05 13:43:17 +0000

    chore: change openapi tags

--- atoma-daemon/src/components/openapi.rs
+++ atoma-daemon/src/components/openapi.rs
@@ -11,20 +11,20 @@ pub fn openapi_routes() -> Router {
     #[derive(OpenApi)]
     #[openapi(
         nest(
-            (path = almost_filled_stacks::ALMOST_FILLED_STACKS_PATH, api = almost_filled_stacks::AlmostFilledStacksOpenApi, tags = ["almost_filled_stacks"]),
-            (path = attestation_disputes::ATTESTATION_DISPUTES_PATH, api = attestation_disputes::AttestationDisputesOpenApi, tags = ["attestation_disputes"]),
-            (path = nodes::NODES_PATH, api = nodes::NodesOpenApi, tags = ["nodes"]),
-            (path = stacks::STACKS_PATH, api = stacks::StacksOpenApi, tags = ["stacks"]),
-            (path = subscriptions::SUBSCRIPTIONS_PATH, api = subscriptions::SubscriptionsOpenApi, tags = ["subscriptions"]),
-            (path = tasks::TASKS_PATH, api = tasks::TasksOpenApi, tags = ["tasks"])
+            (path = almost_filled_stacks::ALMOST_FILLED_STACKS_PATH, api = almost_filled_stacks::AlmostFilledStacksOpenApi, tags = ["Almost filled stacks"]),
+            (path = attestation_disputes::ATTESTATION_DISPUTES_PATH, api = attestation_disputes::AttestationDisputesOpenApi, tags = ["Attestation disputes"]),
+            (path = nodes::NODES_PATH, api = nodes::NodesOpenApi, tags = ["Nodes"]),
+            (path = stacks::STACKS_PATH, api = stacks::StacksOpenApi, tags = ["Stacks"]),
+            (path = subscriptions::SUBSCRIPTIONS_PATH, api = subscriptions::SubscriptionsOpenApi, tags = ["Subscriptions"]),
+            (path = tasks::TASKS_PATH, api = tasks::TasksOpenApi, tags = ["Tasks"])
         ),
         tags(
-            (name = "almost_filled_stacks", description = "Almost filled stacks management"),
-            (name = "attestation_disputes", description = "Attestation disputes management"),
-            (name = "nodes", description = "Nodes management"),
-            (name = "stacks", description = "Stacks management"),
-            (name = "subscriptions", description = "Subscriptions management"),
-            (name = "tasks", description = "Tasks management")
+            (name = "Almost filled stacks", description = "Almost filled stacks management"),
+            (name = "Attestation disputes", description = "Attestation disputes management"),
+            (name = "Nodes", description = "Nodes management"),
+            (name = "Stacks", description = "Stacks management"),
+            (name = "Subscriptions", description = "Subscriptions management"),
+            (name = "Tasks", description = "Tasks management")
         ),
         servers(
             (url = "http://localhost:8080", description = "Local development server")
