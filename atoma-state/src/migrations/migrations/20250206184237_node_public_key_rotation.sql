214c6b7f3c5954a4c0f4617fa6c00d8a64465219
bredamatt <28816406+bredamatt@users.noreply.github.com>
2025-02-06 18:43:11 +0000

    fix: revert migration change and add new migration in stead

--- atoma-state/src/migrations/migrations/20250206184237_node_public_key_rotation.sql
+++ atoma-state/src/migrations/migrations/20250206184237_node_public_key_rotation.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS node_public_key_rotations (
     node_small_id            BIGINT  PRIMARY KEY,
     public_key_bytes         BYTEA    UNIQUE NOT NULL,
-    tdx_quote_bytes          BYTEA    NOT NULL,
+    tee_quote_bytes          BYTEA    NOT NULL,
+    tee_provider             BYTEA    NOT NULL,
     epoch                    BIGINT  NOT NULL
 );
