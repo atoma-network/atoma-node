e7ba54cd7666d19d0f8c6350489c9a4fa30f8be5
Jorge Antonio <matroid@outlook.com>
2025-05-12 17:52:51 +0100

    improve logs

--- atoma-confidential/src/service.rs
+++ atoma-confidential/src/service.rs
@@ -182,7 +182,7 @@ impl AtomaConfidentialCompute {
         tracing::info!(
             target = "atoma-confidential-compute-service",
             event = "new_confidential_compute_service",
-            "New confidential compute service created, with num_devices: {num_devices} and is_cc_supported: {is_cc_supported}"
+            "New confidential compute service created, with num_devices: {num_devices}, is_cc_supported: {is_cc_supported} and is_ppcie_enabled: {is_ppcie_enabled}"
         );
         Ok(Self {
             sui_client,
